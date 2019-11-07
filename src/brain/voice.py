1123d65ccf5b8bd8dcd69c246cd87a9847ddbf50
SkyeOzga <sozga@olin.edu>
2019-11-07 17:18:23 -0500

    VOLUME UP

--- src/brain/voice.py
+++ src/brain/voice.py
@@ -3,7 +3,10 @@ from pydub import AudioSegment
 from pydub.playback import play
 import random
 import time
+from subprocess import call
 
+
+call(["amixer", "-D", "pulse", "sset", "Master", "100%"])
 
 # accelerometer z-axis threshold (m/s^2)
 # beneath this threshold robot is said to be tilting/falling
@@ -16,6 +19,7 @@ if __name__ == "__main__":
     while True:
         accel_data = imu.get_accel_data()
         filename = 'audio_files/Turret_turret_tipped_'
+        print(accel_data)
 
         if accel_data['z'] < Z_ACCEL_THRESHOLD:
             # play screaming sound
